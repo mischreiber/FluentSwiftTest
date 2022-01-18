5030c1f8f24311cc0442b6695e1920167ac6d1f6
Mike Schreiber <schreiber.mike@outlook.com>
2022-01-17 21:41:17 -0800

    Initial Commit

---
+++ Shared/FluentSwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluentSwiftTestApp.swift
+//  Shared
+//
+//  Created by Michael Schreiber on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FluentSwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
